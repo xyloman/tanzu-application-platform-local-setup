943bfbc565b70aeb6fa267be4d4be8430b3f080d
Bryan Kelly <bryan.kelly@cerner.com>
2022-04-14 17:24:56 -0500

    Update tanzu-cluster-essentials to update local system with kapp, imgpkg, kbld and ytt that ship with cluster essentials; update cluster essentials sha

--- tanzu-cluster-essentials.sh
+++ tanzu-cluster-essentials.sh
@@ -2,7 +2,9 @@
 
 set +x
 
-version=${CLUSTER_ESSENTIALS_VERSION:-1.0.0}
+# Reference: https://docs.vmware.com/en/Cluster-Essentials-for-VMware-Tanzu/1.1/cluster-essentials/GUID-deploy.html
+
+version=${CLUSTER_ESSENTIALS_VERSION:-1.1.0}
 glob=${CLUSTER_ESSENTIALS_GLOB:-tanzu-cluster-essentials-linux-amd64-*.tgz}
 tanzu_net_user=$1
 tanzu_net_pass=$2
@@ -17,11 +19,15 @@ rm -fr /tmp/tanzu-cluster-essentials
 mkdir /tmp/tanzu-cluster-essentials
 cat /tmp/${glob} | tar -xvzf - -i -C /tmp/tanzu-cluster-essentials
 
-export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:82dfaf70656b54dcba0d4def85ccae1578ff27054e7533d08320244af7fb0343
+export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:ab0a3539da241a6ea59c75c0743e9058511d7c56312ea3906178ec0f3491f51d
 export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
 export INSTALL_REGISTRY_USERNAME=${tanzu_net_user}
 export INSTALL_REGISTRY_PASSWORD=${tanzu_net_pass}
-cd /tmp/tanzu-cluster-essentials
-./install.sh
 
-cp /tmp/tanzu-cluster-essentials/kapp ~/scripts/kapp
+cd /tmp/tanzu-cluster-essentials
+./install.sh --yes
+
+install /tmp/tanzu-cluster-essentials/kapp ~/scripts/kapp
+install /tmp/tanzu-cluster-essentials/imgpkg ~/scripts/imgpkg
+install /tmp/tanzu-cluster-essentials/kbld ~/scripts/kbld
+install /tmp/tanzu-cluster-essentials/ytt ~/scripts/ytt
