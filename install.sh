daac331eacd77489b19f817c059b6435b9004d42
Bryan Kelly <bryan.kelly@cerner.com>
2022-04-14 17:22:31 -0500

    Update install.sh to relocate images to local registry

--- install.sh
+++ install.sh
@@ -11,8 +11,9 @@ docker inspect -f '{{.NetworkSettings.IPAddress}} dev.local' dev.local | (sudo t
 
 kubectl config use-context kind-${kind_cluster_name}
 
-./tanzu-cluster-essentials.sh ${tanzu_net_user} ${tanzu_net_pass}
 ./tanzu-cli.sh
+./tanzu-cluster-essentials.sh ${tanzu_net_user} ${tanzu_net_pass}
+./tanzu-relocate-images.sh ${tanzu_net_user} ${tanzu_net_pass}
 ./tanzu-application-platform.sh ${tanzu_net_user} ${tanzu_net_pass}
 ./developer-namespace.sh
-./tanzu-application-service-adapter.sh ${tanzu_net_user} ${tanzu_net_pass}
+# ./tanzu-application-service-adapter.sh ${tanzu_net_user} ${tanzu_net_pass}
